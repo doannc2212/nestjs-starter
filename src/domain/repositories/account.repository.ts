866ef268f94d0f86e7a09714a95e867054fe10d9
0x00646f616e6e63 <doannc2212@gmail.com>
2025-01-24 16:44:16 +0700

    migrate to bun

--- src/domain/repositories/account.repository.ts
+++ src/domain/repositories/account.repository.ts
@@ -1,6 +1,6 @@
 import { Account } from '../aggregate/account';
 
-export interface AccountRepository {
+export class AccountRepository {
   save: (account: Account | Account[]) => Promise<void>;
   findById: (id: string) => Promise<Account | null>;
   findByName: (name: string) => Promise<Account[]>;
