e4acd59e194685f2b177739b3540d6724cb1dcc0
0x00646f616e6e63 <doannc2212@gmail.com>
2025-02-09 18:34:08 +0700

    chore: update configuration

---
+++ scripts/generate-protoc.sh
@@ -0,0 +1,5 @@
+protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=. recipe/*.proto --ts_proto_opt=nestJs=true --ts_proto_opt=addGrpcMetadata=true --ts_proto_opt=emitImportedFiles=false 
+
+mv recipe/schema.ts src/presentation/grpc/specifications
+
+echo "Protobuf files generated successfully"
